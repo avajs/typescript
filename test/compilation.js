2bbe2f62a3de14068b55a2c06a12272e0d8e20d7
Mark Wubben <mark@novemberborn.net>
2026-04-12 21:45:40 +0200

    Update dependencies

--- test/compilation.js
+++ test/compilation.js
@@ -6,8 +6,16 @@ import {execaNode} from 'execa';
 import createProviderMacro from './_with-provider.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const withProvider = createProviderMacro('ava-6', '6.0.0', path.join(__dirname, 'fixtures'));
-const withAltProvider = createProviderMacro('ava-6', '6.0.0', path.join(__dirname, 'broken-fixtures'));
+const withProvider = createProviderMacro(
+	'ava-6',
+	'6.0.0',
+	path.join(__dirname, 'fixtures'),
+);
+const withAltProvider = createProviderMacro(
+	'ava-6',
+	'6.0.0',
+	path.join(__dirname, 'broken-fixtures'),
+);
 
 test.before('deleting compiled files', async t => {
 	t.log(await deleteAsync('test/fixtures/typescript/compiled'));
@@ -15,36 +23,48 @@ test.before('deleting compiled files', async t => {
 });
 
 const compile = async provider => ({
-	state: await provider.main({
-		config: {
-			rewritePaths: {
-				'ts/': 'typescript/',
-				'compiled/': 'typescript/compiled/',
+	state: await provider
+		.main({
+			config: {
+				rewritePaths: {
+					'ts/': 'typescript/',
+					'compiled/': 'typescript/compiled/',
+				},
+				compile: 'tsc',
 			},
-			compile: 'tsc',
-		},
-	}).compile(),
+		})
+		.compile(),
 });
 
-test('worker(): load rewritten paths files', withProvider, async (t, provider) => {
-	const {state} = await compile(provider);
-	const {stdout, stderr} = await execaNode(
-		path.join(__dirname, 'fixtures/install-and-load'),
-		[JSON.stringify({state}), path.join(__dirname, 'fixtures/ts', 'file.ts')],
-		{cwd: path.join(__dirname, 'fixtures')},
-	);
-	if (stderr.length > 0) {
-		t.log(stderr);
-	}
+test(
+	'worker(): load rewritten paths files',
+	withProvider,
+	async (t, provider) => {
+		const {state} = await compile(provider);
+		const {stdout, stderr} = await execaNode(
+			path.join(__dirname, 'fixtures/install-and-load'),
+			[
+				JSON.stringify({state}),
+				path.join(__dirname, 'fixtures/ts', 'file.ts'),
+			],
+			{cwd: path.join(__dirname, 'fixtures')},
+		);
+		if (stderr.length > 0) {
+			t.log(stderr);
+		}
 
-	t.snapshot(stdout);
-});
+		t.snapshot(stdout);
+	},
+);
 
 test('worker(): runs compiled files', withProvider, async (t, provider) => {
 	const {state} = await compile(provider);
 	const {stdout, stderr} = await execaNode(
 		path.join(__dirname, 'fixtures/install-and-load'),
-		[JSON.stringify({state}), path.join(__dirname, 'fixtures/compiled', 'index.ts')],
+		[
+			JSON.stringify({state}),
+			path.join(__dirname, 'fixtures/compiled', 'index.ts'),
+		],
 		{cwd: path.join(__dirname, 'fixtures')},
 	);
 	if (stderr.length > 0) {
